c3ac9d77be29a2d134c432ff2dd556e4403da446
Junge <201305945@post.au.dk>
2019-03-11 14:42:08 +0100

    #14 Removing the schema for models we don't save in db

--- src/Models/product.model.ts
+++ src/Models/product.model.ts
@@ -8,35 +8,3 @@ export interface IProduct extends Document {
   description: string;
   price: string;
 }
-
-export const ProductSchema: Schema = new Schema({
-  id: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-    required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  quantity: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: String,
-    required: true
-  }
-});
-
-export const Products: Model<IProduct> = model<IProduct>(
-  "Products",
-  ProductSchema
-);
--- src/Models/prodwloc.model.ts
+++ src/Models/prodwloc.model.ts
@@ -1,24 +1,8 @@
 import { Document, Schema, Model, model } from "mongoose";
 import { ICoord, CoordSchema } from "./location.model";
-import { IProduct, ProductSchema } from "./product.model";
+import { IProduct } from "./product.model";
 
 export interface IProdwloc extends Document {
   product: IProduct;
   location: ICoord;
 }
-
-export const ProdwlocSchema: Schema = new Schema({
-  product: {
-    type: ProductSchema,
-    required: true
-  },
-  location: {
-    type: CoordSchema,
-    required: false
-  }
-});
-
-export const ProdwlocModel: Model<IProdwloc> = model<IProdwloc>(
-  "Prodwloc",
-  ProdwlocSchema
-);
