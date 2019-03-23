c4bd656514e1f8c4f21fcf8970b88d558f7ef40a
Junge <201305945@post.au.dk>
2019-03-23 17:04:36 +0100

    Cors in other services

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,27 @@ import * as cors from "cors";
 
 const PORT = process.env.PORT || 3002;
 
-// Enable cors - default (any) till we know where it will be hosted
-app.use(cors());
+// Enable cors
+app.use(
+  cors({
+    allowedHeaders: ["sessionId", "Content-Type"],
+    exposedHeaders: "sessionId",
+    origin: "*",
+    methods: "GET, POST, PUT, DELETE",
+    preflightContinue: false
+  })
+);
+
+// Middleware for setting cors related headers in response
+app.use(function(req, res, next) {
+  res
+    .header("Access-Control-Allow-Origin", "*")
+    .header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE")
+    .header("Access-Control-Allow-Headers", "SessionId, Content-Type");
+  next();
+});
 
 // Listen on port
 app.listen(PORT, () => {
-    console.log("Express server listening on port " + PORT);
+  console.log("Express server listening on port " + PORT);
 });
